6650847ab349d4ad96c50fbad4849812d34ee8f1
Vinicius Santos <viniciussantos@crewhu.com>
2025-06-10 19:35:05 +0200

    main: fix CI and Release process

--- pkg/tracing/middleware.go
+++ pkg/tracing/middleware.go
@@ -12,57 +12,57 @@ import (
 
 func TracingMiddleware() fiber.Handler {
 	return func(c *fiber.Ctx) error {
-        ctx := SetTraceAtContext(c.UserContext())
-        otelCtx := GetSpanContext(ctx)
-        spanCtx := trace.SpanContextFromContext(otelCtx)
+		ctx := SetTraceAtContext(c.UserContext())
+		otelCtx := GetSpanContext(ctx)
+		spanCtx := trace.SpanContextFromContext(otelCtx)
 
-        c.Locals("traceCtx", ctx)
+		c.Locals("traceCtx", ctx)
 
-        if spanCtx.TraceID().IsValid() {
-            c.Set("X-Trace-ID", spanCtx.TraceID().String())
-            c.Set("X-Span-ID", spanCtx.SpanID().String())
-        }
+		if spanCtx.TraceID().IsValid() {
+			c.Set("X-Trace-ID", spanCtx.TraceID().String())
+			c.Set("X-Span-ID", spanCtx.SpanID().String())
+		}
 
-        var span trace.Span
-        if !spanCtx.IsValid() || !trace.SpanFromContext(otelCtx).IsRecording(){
-            otelCtx, newSpan := GetTracer("fiber").Start(
-                otelCtx, 
-                c.Path(),
-                trace.WithAttributes(
+		var span trace.Span
+		if !spanCtx.IsValid() || !trace.SpanFromContext(otelCtx).IsRecording() {
+			otelCtx, newSpan := GetTracer("fiber").Start(
+				otelCtx,
+				c.Path(),
+				trace.WithAttributes(
 					attribute.String("custom", "yes"),
-                    attribute.String("http.method", c.Method()),
-                    attribute.String("http.url", c.OriginalURL()),
-                ),
-            )
+					attribute.String("http.method", c.Method()),
+					attribute.String("http.url", c.OriginalURL()),
+				),
+			)
 			span = newSpan
 			c.SetUserContext(otelCtx)
-            defer span.End()
+			defer span.End()
 
-        } else {
-            span = trace.SpanFromContext(otelCtx)
-        }
+		} else {
+			span = trace.SpanFromContext(otelCtx)
+		}
 
-        items := 0
-        args := c.Request().URI().QueryArgs()
-        args.VisitAll(func(key, value []byte) {
-            items++
-            span.SetAttributes(
-                attribute.String("http.query."+string(key), string(value)),
-            )
-        })
+		items := 0
+		args := c.Request().URI().QueryArgs()
+		args.VisitAll(func(key, value []byte) {
+			items++
+			span.SetAttributes(
+				attribute.String("http.query."+string(key), string(value)),
+			)
+		})
 
-        span.SetAttributes(
-            attribute.Int("http.query.items", items),
-        )
+		span.SetAttributes(
+			attribute.Int("http.query.items", items),
+		)
 
-        if span.SpanContext().TraceID().IsValid() {
-            log.Println("TraceID:", span.SpanContext().TraceID().String())
-        }
+		if span.SpanContext().TraceID().IsValid() {
+			log.Println("TraceID:", span.SpanContext().TraceID().String())
+		}
 
 		logging.Info(ctx, "URL: %s", c.OriginalURL())
 
-        return c.Next()
-    }
+		return c.Next()
+	}
 }
 
 func GetTraceContext(c *fiber.Ctx) context.Context {
